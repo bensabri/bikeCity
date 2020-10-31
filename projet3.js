c3da0dfab5c302b1ea5d8bfffb251689c695e9cd
sabri <ssabril@ymail.com>
2020-10-31 14:47:56 +0100

    create slider and canvas files
    fixing button reserve

--- projet3.js
+++ projet3.js
@@ -1,58 +1,3 @@
-//Slider
-
-//Selection des boutton next, prev, et playpause
-let nextBtn = document.querySelector('.next').addEventListener('click', nextImage)
-
-let prevBtn = document.querySelector('.prew').addEventListener('click', prevImage)
-let play = document.querySelector('.btnpause').addEventListener('click', playPause)
-
-//Selection de la box qui contient l'img src qui contient les images
-let sliderContainer = document.querySelector('.box');
-
-//Creation d'un tableaux images qui contient les 3 sliders
-let images = ['images/slide1', 'images/slide2', 'images/slide3'];
-let i = 0;
-
-function prevImage() {
-	if(i < images.length && i > 0) {
-		i--
-	} else {
-		i = 2; // I est = a la longeur du tableaux donc on reviens au debut de la boucle  i > 0 on decremente de la dernier photo a la premier
-	}
-	sliderContainer.innerHTML = `<img src=${images[i]}.jpg>`;
-}
-
-// function nextImage
-function nextImage() {
-	if(i < images.length -1) {
-		i++
-	} else {
-		i = 0;
-	}
-	sliderContainer.innerHTML = `<img src=${images[i]}.jpg>`;
-}
-
-document.querySelector('body').addEventListener('keydown', (e) => {
-	if(e.keyCode == 39) {
-		nextImage()
-	} else if(e.keyCode == 37) {
-		prevImage()
-	}
-})
-	
-// function prevImage
-
-let timer = setInterval(nextImage, 5000);
-
-function playPause() {
-	if(timer) {
-		clearInterval(timer)
-		timer = null
-	} else {
-		setInterval(nextImage, 5000);
-	} 
-}
-
 // Mouse drawing
 let canvas = document.querySelector('#draw');
 let ctx = canvas.getContext('2d');
@@ -225,12 +170,12 @@ document.querySelector('.cancel').addEventListener('click', showMap) // When cli
 	}		
 
 let input = document.querySelector("input");	// disable input button if empty form 
-//btnsend.disabled = true;
-//input.addEventListener('change', champRempli)
+btnsend.disabled = true;
+input.addEventListener('change', champRempli)
 
-//function champRempli() {
+function champRempli() {
 	
-	/*if(document.querySelector('input').value === '') {
+	if(document.querySelector('input').value === '') {
 		btnsend.disabled = true
 	} else {
 		btnsend.disabled = false
---
+++ slider.js
@@ -0,0 +1,51 @@
+// Slider //
+
+//Selection des boutton next, prev, et playpause
+let nextBtn = document.querySelector('.next').addEventListener('click', nextImage)
+
+let prevBtn = document.querySelector('.prew').addEventListener('click', prevImage)
+let play = document.querySelector('.btnpause').addEventListener('click', playPause)
+
+//Selection de la box qui contient l'img src qui contient les images
+let sliderContainer = document.querySelector('.box');
+
+//Creation d'un tableaux images qui contient les 3 sliders
+let images = ['images/slide1', 'images/slide2', 'images/slide3'];
+let i = 0;
+
+function prevImage() {  // function prevImage
+    if(i < images.length && i > 0) {
+        i--
+    } else {
+        i = 2; // I est = a la longeur du tableaux donc on reviens au debut de la boucle  i > 0 on decremente de la dernier photo a la premier
+    }
+    sliderContainer.innerHTML = `<img src=${images[i]}.jpg>`;
+}
+function nextImage() {  // function nextImage
+    if(i < images.length -1) {
+        i++
+    } else {
+        i = 0;
+    }
+    sliderContainer.innerHTML = `<img src=${images[i]}.jpg>`;
+}
+
+document.querySelector('body').addEventListener('keypress', (e) => { // switch right left images with arrow keyboard 
+    if(e.keyCode == 39) {
+        nextImage()
+    } else if (e.keyCode == 37) {
+        prevImage()
+    }
+})
+// create play auto every 5 secondes
+let timer = setInterval(nextImage, 5000);
+
+function playPause() {
+    if(timer) {
+       clearInterval(timer)
+       timer = null; 
+    } else {
+        setInterval(nextImage, 5000)
+    }
+}
+
